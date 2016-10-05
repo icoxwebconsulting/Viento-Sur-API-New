4e7f8dd25eace2a2a57d947846e2e7bc688f0f1d
mauro <mauro@icox.com.ar>
2016-10-05 15:16:22 -0300

    test curl

--- src/VientoSur/App/AppBundle/Controller/HotelController.php
+++ src/VientoSur/App/AppBundle/Controller/HotelController.php
@@ -49,6 +49,10 @@ class HotelController extends Controller {
         
         //step1
         $postvars = json_encode($params);
+        
+        echo $postvars;
+        exit();
+        
         $cSession = curl_init();
         curl_setopt($cSession, CURLOPT_URL, $url);
         curl_setopt($cSession, CURLOPT_POST, true);
