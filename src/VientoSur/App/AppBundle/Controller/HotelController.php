f13273b170b96650b48cc265dd6fae500b526b55
mauro <mauro@icox.com.ar>
2016-10-11 14:48:25 -0300

    update hotel form

--- src/VientoSur/App/AppBundle/Controller/HotelController.php
+++ src/VientoSur/App/AppBundle/Controller/HotelController.php
@@ -342,10 +342,9 @@ class HotelController extends Controller {
         $formNewPay = $this->createFormBuilder($formBooking);
         
         foreach ($formBooking['dictionary']['form_choices'][1]['passengers'] AS $k=>$passengers){
-            echo $passengers['first_name']['type'];
-            $formNewPay->add("first_name_$k", $passengers['first_name']['type']);
-            $formNewPay->add("last_name_$k", $passengers['last_name']['type']);
-            $formNewPay->add("document_number_$k", $passengers['document_number']['type']);
+            $formNewPay->add("first_name_$k", 'text');
+            $formNewPay->add("last_name_$k", 'text');
+            $formNewPay->add("document_number_$k", 'text');
         }
         exit();
         
