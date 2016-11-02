428499be44375c024178178a5e4091dab9e42711
David De Freitas <davidjdr@icox.com>
2016-11-02 16:33:39 -0400

    Agregado controlador para búsquedas de vuelo. Habilitado formulario para búsqueda de vuelo.

---
+++ src/VientoSur/App/AppBundle/Controller/FlightController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace VientoSur\App\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ *
+ * @Route("/vuelos")
+ */
+class FlightController extends Controller
+{
+
+}
