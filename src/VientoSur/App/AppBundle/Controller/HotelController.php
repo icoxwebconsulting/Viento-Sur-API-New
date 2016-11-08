1c62b7d84a5e6e0b6d93350619f5964ace6479ef
David De Freitas <davidjdr@icox.com>
2016-11-08 15:18:38 -0400

    Agregado autocomplete en formulario de pago entre otros cambios.

--- src/VientoSur/App/AppBundle/Controller/HotelController.php
+++ src/VientoSur/App/AppBundle/Controller/HotelController.php
@@ -232,7 +232,7 @@ class HotelController extends Controller {
         $session = $request->getSession();
         $urlParams = array(
             'language' => 'es',
-            'currency' => 'USD'
+            'currency' => 'ARS'
         );
 
         $despegar = $this->get('despegar');
@@ -515,33 +515,5 @@ class HotelController extends Controller {
         curl_close($cSession);
 
         return $results;
-    }
-
-    /**
-     *
-     * @Route("/autocomplete/", name="hotel_autocomplete")
-     * @Method("GET")
-     */
-    public function autoCompleteHotelAction(Request $request)
-    {
-        $type = 'HOTELS';
-        $urlParams = [
-            'query' => $request->get('query'),
-            'product' => $type,
-            'locale' => 'es-AR',
-            'city_result' => '10'
-        ];
-
-        $results = $this->get('despegar')->autocomplete($urlParams);
-        $response = [];
-        if ($results && !isset($results['code'])) {
-            foreach ($results as $item) {
-                $city = Array();
-                $city["value"] = $item["description"];
-                $city["data"] = substr($item["id"], 5);
-                $response[] = $city;
-            }
-        }
-        return new JsonResponse(array("suggestions" => $response, 'query' => $request->get('query'), 'test' => $results));
     }
 }
--- src/VientoSur/App/AppBundle/Controller/IndexController.php
+++ src/VientoSur/App/AppBundle/Controller/IndexController.php
@@ -13,6 +13,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class IndexController extends Controller
 {
@@ -36,10 +37,10 @@ class IndexController extends Controller
 
         $languages['es']['icon'] = 'bundles/vientosurappapp/images/fl-ar.png';
         $languages['es']['name'] = 'Español';
-        $languages['en']['icon'] = 'bundles/vientosurappapp/images/fl-en.png';
-        $languages['en']['name'] = 'English';
-        $languages['pt']['icon'] = 'bundles/vientosurappapp/images/flag_pt.png';
-        $languages['pt']['name'] = 'Portuguese';
+        //$languages['en']['icon'] = 'bundles/vientosurappapp/images/fl-en.png';
+        //$languages['en']['name'] = 'English';
+        //$languages['pt']['icon'] = 'bundles/vientosurappapp/images/flag_pt.png';
+        //$languages['pt']['name'] = 'Portuguese';
 
         $currencies['ars']['name'] = "AR$";
         //$currencies['usd']['name'] = "USD";
@@ -66,4 +67,88 @@ class IndexController extends Controller
         );
     }
 
+    /**
+     *
+     * @Route("/autocomplete/", name="hotel_autocomplete")
+     * @Method("GET")
+     */
+    public function autoCompleteHotelAction(Request $request)
+    {
+        $type = 'HOTELS';
+        $urlParams = [
+            'query' => $request->get('query'),
+            'product' => $type,
+            'locale' => 'es-AR',
+            'city_result' => '10'
+        ];
+
+        $results = $this->get('despegar')->autocomplete($urlParams);
+        $response = [];
+        if ($results && !isset($results['code'])) {
+            foreach ($results as $item) {
+                $city = Array();
+                $city["value"] = $item["description"];
+                $city["data"] = substr($item["id"], 5);
+                $response[] = $city;
+            }
+        }
+        return new JsonResponse(array("suggestions" => $response, 'query' => $request->get('query'), 'test' => $results));
+    }
+
+    /**
+     *
+     * @Route("/autocomplete-state/", name="state_autocomplete")
+     * @Method("GET")
+     */
+    public function autoCompleteStateAction(Request $request)
+    {
+        $type = 'HOTELS';
+        $urlParams = [
+            'query' => $request->get('query'),
+            'product' => $type,
+            'locale' => 'es-AR',
+            'administrative_division_result' => '10'
+        ];
+
+        $results = $this->get('despegar')->autocomplete($urlParams);
+        $response = [];
+        if ($results && !isset($results['code'])) {
+            foreach ($results as $item) {
+                $city = [
+                    'value' => $item["description"],
+                    'data' => $item['item']["id"]
+                ];
+                $response[] = $city;
+            }
+        }
+        return new JsonResponse(array("suggestions" => $response, 'query' => $request->get('query'), 'test' => $results));
+    }
+
+    /**
+     *
+     * @Route("/autocomplete-city", name="city_autocomplete")
+     * @Method("GET")
+     */
+    public function autoCompleteCityAction(Request $request)
+    {
+        $type = 'HOTELS';
+        $urlParams = [
+            'query' => $request->get('query'),
+            'product' => $type,
+            'locale' => 'es-AR',
+            'city_result' => '10'
+        ];
+
+        $results = $this->get('despegar')->autocomplete($urlParams);
+        $response = [];
+        if ($results && !isset($results['code'])) {
+            foreach ($results as $item) {
+                $city = Array();
+                $city["value"] = $item["description"];
+                $city["data"] = substr($item["id"], 5);
+                $response[] = $city;
+            }
+        }
+        return new JsonResponse(array("suggestions" => $response, 'query' => $request->get('query'), 'test' => $results));
+    }
 }
