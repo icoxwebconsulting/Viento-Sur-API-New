900138f3ca4274b224ae6073ce31d771ed41dfc3
mauro <mauro@icox.com>
2018-05-17 11:14:28 -0300

    update actividades

---
+++ src/BackendBundle/Controller/ActivityController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BackendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
+use VientoSur\App\AppBundle\Entity\Hotel;
+use VientoSur\App\AppBundle\Entity\Room;
+
+/**
+ * @Route("/{_locale}/activity", requirements={"_locale": "es|en|pt"}, defaults={"_locale": "es"})
+ */
+class ActivityController extends Controller
+{
+     /**
+     * @Security("has_role('ROLE_HOTELIER')")
+     * @Route("/", name="actyvity")
+     * @Method("GET")
+     * @return array
+     */
+    public function indexAction()
+    {
+        return $this->render('admin/index.html.twig');
+    }
+}
