5c4cc8c793472b4248e0cfdad2b5f1f255e3aa12
mauro <mauro@icox.com>
2018-06-08 11:54:32 -0300

    add from activitytype

---
+++ src/BackendBundle/Form/ActivityType.php
@@ -0,0 +1,339 @@
+<?php
+
+namespace BackendBundle\Form;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use VientoSur\App\AppBundle\Entity\ActivityAgency;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use BackendBundle\Form\UserType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ActivityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'name',
+                TextType::class,
+                array(
+                    'label' => false,
+                    'required' => true
+                )    
+            )
+            ->add(
+                'namePt',
+                TextType::class,
+                array(
+                    'mapped' => false,
+                    'required' => false
+                )
+            )
+            ->add(
+                'nameEn',
+                TextType::class,
+                array(
+                    'mapped' => false,
+                    'required' => false
+                )
+            )
+            ->add(
+                'description',
+                TextareaType::class,
+                array(
+                    'label' => false,
+                    'mapped' => true,
+                    'attr' => array(
+                        "minlength" => 1, //Longitud minima
+                    )
+                )
+            )
+            ->add(
+                'descriptionPt',
+                TextareaType::class,
+                array(
+                    'mapped' => false,
+                    'required' => false
+                )
+            )
+            ->add(
+                'descriptionEn',
+                TextareaType::class,
+                array(
+                    'mapped' => false,
+                    'required' => false
+                )
+            )
+            ->add(
+                'address',
+                TextType::class
+            )
+            ->add(
+                'latitude',
+                HiddenType::class
+            )
+            ->add(
+                'longitude',
+                HiddenType::class
+            )  
+            ->add(
+                'price',
+                TextType::class
+            )    
+            ->add(
+                 'availability',
+                 'checkbox',
+                 array(
+                    'attr' => array('checked'   => 'checked'),
+                )   
+            )
+            ->add(
+                'capacity_for_shift',
+                ChoiceType::class,
+                array(
+                    'choices' => ['1'=>'1',
+                                  '2'=>'2',
+                                  '3'=>'3',
+                                  '4'=>'4',
+                                  '5'=>'5',
+                                  '6'=>'6',
+                                  '7'=>'7',
+                                  '8'=>'8',
+                                  '9'=>'9',
+                                  '10'=>'10',
+                                  '11'=>'11',
+                                  '12'=>'12',
+                                  '13'=>'13',
+                                  '14'=>'14',
+                                  '15'=>'15',
+                                  '16'=>'16',
+                                  '17'=>'17',
+                                  '18'=>'18',
+                                  '19'=>'19',
+                                  '20'=>'20',]
+                )
+            )
+            ->add(
+                'duration',
+                ChoiceType::class,
+                array(
+                    'choices' => ['1'=>'1',
+                                  '2'=>'2',
+                                  '3'=>'3',
+                                  '4'=>'4',
+                                  '5'=>'5',
+                                  '6'=>'6',
+                                  '7'=>'7',
+                                  '8'=>'8',
+                                  '9'=>'9',
+                                  '10'=>'10',
+                                  '11'=>'11',
+                                  '12'=>'12',
+                                  '13'=>'13',
+                                  '14'=>'14',
+                                  '15'=>'15',
+                                  '16'=>'16',
+                                  '17'=>'17',
+                                  '18'=>'18',
+                                  '19'=>'19',
+                                  '20'=>'20',]
+                )
+            )  
+            ->add(
+                'pick_up_am',
+                ChoiceType::class,
+                array(
+                    'choices' => ['00:00'=>'00:00',
+                                  '01:00'=>'01:00',  
+                                  '02:00'=>'02:00',
+                                  '03:00'=>'03:00',
+                                  '04:00'=>'04:00',
+                                  '05:00'=>'05:00',
+                                  '06:00'=>'06:00',
+                                  '07:00'=>'07:00',
+                                  '08:00'=>'08:00',
+                                  '09:00'=>'09:00',
+                                  '10:00'=>'10:00',
+                                  '11:00'=>'11:00',
+                                  '12:00'=>'12:00']
+                )
+            )
+            ->add(
+                'from_am',
+                ChoiceType::class,
+                array(
+                    'choices' => ['00:00'=>'00:00',
+                                  '01:00'=>'01:00',  
+                                  '02:00'=>'02:00',
+                                  '03:00'=>'03:00',
+                                  '04:00'=>'04:00',
+                                  '05:00'=>'05:00',
+                                  '06:00'=>'06:00',
+                                  '07:00'=>'07:00',
+                                  '08:00'=>'08:00',
+                                  '09:00'=>'09:00',
+                                  '10:00'=>'10:00',
+                                  '11:00'=>'11:00',
+                                  '12:00'=>'12:00']
+                )
+            )  
+            ->add(
+                'to_am',
+                ChoiceType::class,
+                array(
+                    'choices' => ['00:00'=>'00:00',
+                                  '01:00'=>'01:00',  
+                                  '02:00'=>'02:00',
+                                  '03:00'=>'03:00',
+                                  '04:00'=>'04:00',
+                                  '05:00'=>'05:00',
+                                  '06:00'=>'06:00',
+                                  '07:00'=>'07:00',
+                                  '08:00'=>'08:00',
+                                  '09:00'=>'09:00',
+                                  '10:00'=>'10:00',
+                                  '11:00'=>'11:00',
+                                  '12:00'=>'12:00']
+                )
+            )    
+            ->add(
+                'pick_up_pm',
+                ChoiceType::class,
+                array(
+                    'choices' => ['13:00'=>'13:00',
+                                  '14:00'=>'14:00',  
+                                  '15:00'=>'15:00',
+                                  '16:00'=>'16:00',
+                                  '17:00'=>'17:00',
+                                  '18:00'=>'18:00',
+                                  '19:00'=>'19:00',
+                                  '20:00'=>'20:00',
+                                  '21:00'=>'21:00',
+                                  '22:00'=>'22:00',
+                                  '23:00'=>'23:00',]
+                )
+            ) 
+            ->add(
+                'from_pm',
+                ChoiceType::class,
+                array(
+                    'choices' => ['13:00'=>'13:00',
+                                  '14:00'=>'14:00',  
+                                  '15:00'=>'15:00',
+                                  '16:00'=>'16:00',
+                                  '17:00'=>'17:00',
+                                  '18:00'=>'18:00',
+                                  '19:00'=>'19:00',
+                                  '20:00'=>'20:00',
+                                  '21:00'=>'21:00',
+                                  '22:00'=>'22:00',
+                                  '23:00'=>'23:00',]
+                )
+            )
+            ->add(
+                'to_pm',
+                ChoiceType::class,
+                array(
+                    'choices' => ['13:00'=>'13:00',
+                                  '14:00'=>'14:00',  
+                                  '15:00'=>'15:00',
+                                  '16:00'=>'16:00',
+                                  '17:00'=>'17:00',
+                                  '18:00'=>'18:00',
+                                  '19:00'=>'19:00',
+                                  '20:00'=>'20:00',
+                                  '21:00'=>'21:00',
+                                  '22:00'=>'22:00',
+                                  '23:00'=>'23:00',]
+                )
+            ) 
+            ->add(
+                'from_all',
+                ChoiceType::class,
+                array(
+                    'choices' => [
+                                  '00:00'=>'00:00',
+                                  '01:00'=>'01:00',  
+                                  '02:00'=>'02:00',
+                                  '03:00'=>'03:00',
+                                  '04:00'=>'04:00',
+                                  '05:00'=>'05:00',
+                                  '06:00'=>'06:00',
+                                  '07:00'=>'07:00',
+                                  '08:00'=>'08:00',
+                                  '09:00'=>'09:00',
+                                  '10:00'=>'10:00',
+                                  '11:00'=>'11:00',
+                                  '12:00'=>'12:00',  
+                                  '13:00'=>'13:00',
+                                  '14:00'=>'14:00',  
+                                  '15:00'=>'15:00',
+                                  '16:00'=>'16:00',
+                                  '17:00'=>'17:00',
+                                  '18:00'=>'18:00',
+                                  '19:00'=>'19:00',
+                                  '20:00'=>'20:00',
+                                  '21:00'=>'21:00',
+                                  '22:00'=>'22:00',
+                                  '23:00'=>'23:00',]
+                )
+            )
+            ->add(
+                'to_all',
+                ChoiceType::class,
+                array(
+                    'choices' => ['00:00'=>'00:00',
+                                  '01:00'=>'01:00',  
+                                  '02:00'=>'02:00',
+                                  '03:00'=>'03:00',
+                                  '04:00'=>'04:00',
+                                  '05:00'=>'05:00',
+                                  '06:00'=>'06:00',
+                                  '07:00'=>'07:00',
+                                  '08:00'=>'08:00',
+                                  '09:00'=>'09:00',
+                                  '10:00'=>'10:00',
+                                  '11:00'=>'11:00',
+                                  '12:00'=>'12:00',  
+                                  '13:00'=>'13:00',
+                                  '14:00'=>'14:00',  
+                                  '15:00'=>'15:00',
+                                  '16:00'=>'16:00',
+                                  '17:00'=>'17:00',
+                                  '18:00'=>'18:00',
+                                  '19:00'=>'19:00',
+                                  '20:00'=>'20:00',
+                                  '21:00'=>'21:00',
+                                  '22:00'=>'22:00',
+                                  '23:00'=>'23:00',]
+                )
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'VientoSur\App\AppBundle\Entity\ActivityAgency',
+            'id' => null,
+            'cascade_validation' => true
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_activity_agency';
+    }
+}
